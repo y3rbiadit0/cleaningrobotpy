eb800ab1e310c73235cbf9a25ae2a5cde1314a79
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-03 11:51:59 +0100

    US1 - Green Phase

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -8,14 +8,25 @@ from src.cleaning_robot import CleaningRobot
 
 class TestCleaningRobot(TestCase):
 
-    @patch.object(GPIO, "input")
-    def test_initialize_robot(self, mock_object: Mock):
+
+    def test_initialize_robot(self):
 
         expected_init_coordinates = "(0,0,N)"
         cleaning_robot = CleaningRobot()
         cleaning_robot.initialize_robot()
 
         status = cleaning_robot.robot_status()
+        self.assertEqual(status, expected_init_coordinates)
+
+    def test_robot_status(self):
+        expected_init_coordinates = "(2,1,S)"
+
+        cleaning_robot = CleaningRobot()
+        cleaning_robot.pos_x = "2"
+        cleaning_robot.pos_y = "1"
+        cleaning_robot.heading = "S"
+        status = cleaning_robot.robot_status()
+
         self.assertEqual(status, expected_init_coordinates)
 
 
