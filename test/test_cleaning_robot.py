537d0e6dbb5e8edb900111c6e6b7f56e64359c2d
Merenda Franco N <merenda.franco83@gmail.com>
2024-12-03 12:08:30 +0100

    US2 - Red Phase

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -29,4 +29,35 @@ class TestCleaningRobot(TestCase):
 
         self.assertEqual(status, expected_init_coordinates)
 
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_battery_greater_than_ten_percent(self, mock_gpio_output: Mock, mock_charge: Mock):
+        mock_charge.return_value = 11
+        cleaning_robot = CleaningRobot()
+        cleaning_robot.manage_cleaning_system()
+
+        expected_recharge_led_pin_call = call(cleaning_robot.RECHARGE_LED_PIN, GPIO.LOW)
+        expected_cleaning_system_call = call(cleaning_robot.CLEANING_SYSTEM_PIN, GPIO.HIGH)
+        mock_gpio_output.assert_has_calls([expected_cleaning_system_call, expected_recharge_led_pin_call])
+        self.assertTrue(cleaning_robot.cleaning_system_on)
+        self.assertFalse(cleaning_robot.recharge_led_on)
+
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_battery_greater_than_ten_percent(self, mock_gpio_output: Mock, mock_charge: Mock):
+        mock_charge.return_value = 9
+        cleaning_robot = CleaningRobot()
+        cleaning_robot.manage_cleaning_system()
+
+        expected_recharge_led_pin_call = call(cleaning_robot.RECHARGE_LED_PIN, GPIO.HIGH)
+        expected_cleaning_system_call = call(cleaning_robot.CLEANING_SYSTEM_PIN, GPIO.LOW)
+        mock_gpio_output.assert_has_calls([expected_cleaning_system_call, expected_recharge_led_pin_call])
+        self.assertFalse(cleaning_robot.cleaning_system_on)
+        self.assertTrue(cleaning_robot.recharge_led_on)
+
+
+
+
+
+
 
